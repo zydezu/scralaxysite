d5fc1119783f2429c5a12b4a42a24814bfbbd5c7
HopHopHasan <50119098+HopHopHasan@users.noreply.github.com>
2022-10-30 20:49:06 +0000

    update

---
+++ scripts/title.js
@@ -0,0 +1,44 @@
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+let keysPressed = {};
+var titleHovered = 0;
+
+
+document.addEventListener('keydown', (event) => {
+    keysPressed[event.key] = true;
+    if (keysPressed['z'] && event.key == 'x' || keysPressed['x'] && event.key == 'z') {
+        document.getElementById("pressZX").src = "assets/titlescreen/ZXGlow.svg";
+        //var pageToGo = "blog.html";
+        //window.location.href = pageToGo;
+    } else if (event.key == 'z'){
+        document.getElementById("pressZX").src = "assets/titlescreen/ZGlow.svg";
+    } else if (event.key == 'x'){
+        document.getElementById("pressZX").src = "assets/titlescreen/XGlow.svg";
+    }
+});
+document.addEventListener('keyup', (event) => {
+    keysPressed = {};
+    document.getElementById("pressZX").src = "assets/titlescreen/PressZX.svg";
+ });
+
+function spawnSparkle() {
+    var img = document.createElement('img');
+    img.src = 'assets/titlescreen/sparkle.svg';
+    img.id = 'sparkle';
+    img.style.top = random(25,50) + "%";
+    img.style.left = random(25,75) + "%";
+    document.getElementById('flash').appendChild(img);
+    document.getElementById('sparkle').classList.add("sparkle");
+    setTimeout(deleteSparkle, 1000);
+}
+
+function sparkle(){
+    setInterval(spawnSparkle, random(650 - titleHovered, 1750 - titleHovered));
+}
+
+function deleteSparkle(){
+    var elements = document.getElementsByClassName('sparkle');
+    elements[0].remove();
+}
